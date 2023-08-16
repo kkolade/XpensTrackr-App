1b6e0dc93ed087a275eb0ae791981d418ab9244b
Kola Kolade <kolakolade@gmail.com>
2023-08-16 23:17:15 +0100

    Fix error due to sign-up form update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
     # Other permitted parameters
   end
 end
