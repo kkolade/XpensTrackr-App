27d3a97dd8fa8307b2ee23def2b5cd37e9c69370
Kola Kolade <kolakolade@gmail.com>
2023-08-16 22:47:35 +0100

    Add devise for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :categorized_categories, class_name: 'Category', foreign_key: 'author_id'
   has_many :categorized_transactions, class_name: 'Transaction', foreign_key: 'author_id'
   has_one_attached :picture
