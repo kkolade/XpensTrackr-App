77a0a61dfc8295c71c0b7054127b1290cf57d060
Kola Kolade <kolakolade@gmail.com>
2023-08-16 22:23:47 +0100

    Add association and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :categorized_categories, class_name: 'Category', foreign_key: 'author_id'
+  has_many :categorized_transactions, class_name: 'Transaction', foreign_key: 'author_id'
+  has_one_attached :picture
+
+  validates :name, presence: true
 end
